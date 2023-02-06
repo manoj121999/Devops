a9752b45c180b483dc52453a71d5944330d741f5
Manoj Mangena <70632603+manoj121999@users.noreply.github.com>
2023-02-06 11:34:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TASK=`aws ecs list-tasks --cluster DEMO --service Demo_app --desired-status RUNNING --output text | awk '{print $2}'`
-aws ecs stop-task --cluster DEMO --task $TASK
+TASK=`sudo aws ecs list-tasks --cluster DEMO --service Demo_app --desired-status RUNNING --region ap-south-1--output text | awk '{print $2}'`
+sudo aws ecs stop-task --cluster DEMO --task $TASK
 echo "Task stopped successfully"
